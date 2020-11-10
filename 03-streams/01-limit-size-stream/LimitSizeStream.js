9a2b4933a7ddb2ca66ba629bd217241fa8e4aae1
Султанова Марина Рафаилевна <MSultanova@alfabank.ru>
2020-11-10 23:01:13 +0300

    add solution for 3-module

--- 03-streams/01-limit-size-stream/LimitSizeStream.js
+++ 03-streams/01-limit-size-stream/LimitSizeStream.js
@@ -4,10 +4,19 @@ const LimitExceededError = require('./LimitExceededError');
 class LimitSizeStream extends stream.Transform {
   constructor(options) {
     super(options);
+
+    this.limit = Number(options.limit) || 2;
+    this.sizeStream = 0;
   }
 
   _transform(chunk, encoding, callback) {
+    this.sizeStream += chunk.byteLength;
 
+    if (this.limit < this.sizeStream) {
+      callback(new LimitExceededError(), null);
+    }
+
+    callback(null, chunk);
   }
 }
 
--- 03-streams/02-line-split-stream/LineSplitStream.js
+++ 03-streams/02-line-split-stream/LineSplitStream.js
@@ -2,15 +2,32 @@ const stream = require('stream');
 const os = require('os');
 
 class LineSplitStream extends stream.Transform {
-  constructor(options) {
-    super(options);
-  }
+    constructor(options) {
+        super(options);
 
-  _transform(chunk, encoding, callback) {
-  }
+        this.line = '';
 
-  _flush(callback) {
-  }
+    }
+
+    _transform(chunk, encoding, callback) {
+        const charArray = chunk.toString().split('');
+
+        for (const char of charArray) {
+            if (char === os.EOL) {
+                this.push(this.line);
+                this.line = ''
+                continue;
+            }
+
+            this.line += char;
+
+        }
+        callback();
+    }
+
+    _flush(callback) {
+        callback(null, this.line)
+    }
 }
 
 module.exports = LineSplitStream;
